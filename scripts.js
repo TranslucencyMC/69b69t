1fc3c4e13c586ff6069322e015b69f598f9db738
PenguinDEV-aka-Inet_Galaxy <106496472+inet-software-aka-me-being-bored@users.noreply.github.com>
2022-10-31 20:52:22 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function addbutton() {
+    alert("Do you want to actually add wayland? well U SHOULD")
+    location.href = "https://discord.com/oauth2/authorize?client_id=1030796760093823027&permissions=1133584&scope=bot";
+   
+}
+
+function serverbutton() {
+    alert("Do you want to actually add joun the WayLand Discord? well U SHOULD")
+    location.href = "https://discord.gg/XJRw9Q9g";
+   
+}
+
+function homebutton() {
+    location.href = "/index.htm";
+   
+}
+
